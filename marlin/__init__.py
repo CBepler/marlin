90b6941d3f9974daf73b523c015146fbdff55a29
Christian Bepler <bepler135@gmail.com>
2024-12-04 15:41:47 +0000

    adjust to int2xbf16

--- marlin/__init__.py
+++ marlin/__init__.py
@@ -21,11 +21,11 @@ import torch.nn as nn
 import marlin_cuda
 
 def mul(A, B, C, s, workspace, thread_k=-1, thread_n=-1, sms=-1, max_par=16):
-    """Marlin FP16xINT4 multiply; can be used within `torch.compile`.
-    @A: `torch.half` input matrix of shape `(m, k)` in standard row-major layout
+    """Marlin BF16xINT2 multiply; can be used within `torch.compile`.
+    @A: `torch.bfloat16` input matrix of shape `(m, k)` in standard row-major layout
     @B: `torch.int` weight matrix of original shape `(k, n)` in Marlin format; see `Layer.pack()`
-    @C: `torch.half` out matrix of shape `(m, n)` in standard row-major layout
-    @s: `torch.half` scales of shape `(m / groupsize, n)`
+    @C: `torch.bfloat16` out matrix of shape `(m, n)` in standard row-major layout
+    @s: `torch.bfloat16` scales of shape `(m / groupsize, n)`
     @workspace: `torch.int` tensor with at least `n / 128 * max_par` entries that are all zero
     @thread_k: `k` size of a thread_tile in `B` (can usually be left as auto -1)
     @thread_n: `n` size of a thread_tile in `B` (can usually be left as auto -1)
@@ -54,22 +54,22 @@ def _get_perms():
             perm.extend([p + 256 * j for p in perm1])
 
     perm = np.array(perm)
-    interleave = np.array([0, 2, 4, 6, 1, 3, 5, 7])
-    perm = perm.reshape((-1, 8))[:, interleave].ravel()
+    interleave = np.array([0, 2, 4, 6, 8, 10, 12, 14, 1, 3, 5, 7, 9, 11, 13, 15])
+    perm = perm.reshape((-1, 16))[:, interleave].ravel()
     perm = torch.from_numpy(perm)
     scale_perm = []
-    for i in range(8):
-        scale_perm.extend([i + 8 * j for j in range(8)])
+    for i in range(16):
+        scale_perm.extend([i + 16 * j for j in range(16)])
     scale_perm_single = []
-    for i in range(4):
-        scale_perm_single.extend([2 * i + j for j in [0, 1, 8, 9, 16, 17, 24, 25]])
+    for i in range(8):
+        scale_perm_single.extend([2 * i + j for j in [0, 1, 16, 17, 32, 33, 48, 49]])
     return perm, scale_perm, scale_perm_single
 
 _perm, _scale_perm, _scale_perm_single = _get_perms()
 
 
 class Layer(nn.Module):
-    """PyTorch compatible Marlin layer; 4-bit (symmetric grouped) linear layer without bias."""
+    """PyTorch compatible Marlin layer; 2-bit (symmetric grouped) linear layer without bias."""
 
     def __init__(self, infeatures, outfeatures, groupsize=-1):
         """Create an empty Marlin layer.
@@ -89,8 +89,8 @@ class Layer(nn.Module):
         self.k = infeatures
         self.n = outfeatures
         self.groupsize = groupsize
-        self.register_buffer('B', torch.empty((self.k // 16, self.n * 16 // 8), dtype=torch.int))
-        self.register_buffer('s', torch.empty((self.k // groupsize, self.n), dtype=torch.half))
+        self.register_buffer('B', torch.empty((self.k // 16, self.n * 16 // 16), dtype=torch.int))
+        self.register_buffer('s', torch.empty((self.k // groupsize, self.n), dtype=torch.bfloat16))
         # 128 is currently the minimum `tile_n`, hence it gives the maximum workspace size; 16 is the default `max_par`
         self.register_buffer('workspace', torch.zeros(self.n // 128 * 16, dtype=torch.int), persistent=False)
 
@@ -101,13 +101,13 @@ class Layer(nn.Module):
 
     def pack(self, linear, scales):
         """Pack a fake-quantized linear layer into this actual Marlin representation.
-        @linear: fake-quantized `torch.nn.Linear` layer to convert (must be of type `torch.half`)
+        @linear: fake-quantized `torch.nn.Linear` layer to convert (must be of type `torch.bfloat16`)
         @scales: corresponding quantization scales of shape `(infeatures, groups)`
         """ 
-        if linear.weight.dtype != torch.half:
-            raise ValueError('Only `torch.half` weights are supported.')
+        if linear.weight.dtype != torch.bfloat16:
+            raise ValueError('Only `torch.bfloat16` weights are supported.')
         tile = 16
-        maxq = 2 ** 4 - 1
+        maxq = 2 ** 2 - 1
         s = scales.t()
         w = linear.weight.data.t()
         if self.groupsize != self.k:
@@ -131,10 +131,10 @@ class Layer(nn.Module):
         w = w.reshape((self.k // tile, self.n * tile))
         res = w
         res = res.reshape((-1, _perm.numel()))[:, _perm].reshape(res.shape)
-        q = np.zeros((res.shape[0], res.shape[1] // 8), dtype=np.uint32)
+        q = np.zeros((res.shape[0], res.shape[1] // 16), dtype=np.uint32) #16 values packed into each uint32
         res = res.cpu().numpy().astype(np.uint32)
-        for i in range(8):
-            q |= res[:, i::8] << 4 * i
+        for i in range(16):
+            q |= res[:, i::16] << 2 * i  #grabs every 16th bit and then shifts by 2 for packing
         q = torch.from_numpy(q.astype(np.int32)).to(w.device)
         self.B[:, :] = q.to(self.B.device)
         self.s[:, :] = s.to(self.s.device)
